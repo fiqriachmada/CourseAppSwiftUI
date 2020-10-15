e52789da8e97fda4b805f63c755cc496792e440b
ACHMADA FIQRI A. RASYAD <38800157+fiqriachmada@users.noreply.github.com>
2020-10-15 21:18:59 +0700

    Add files via upload

---
+++ CourseApp/CourseApp/CourseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseAppApp.swift
+//  CourseApp
+//
+//  Created by Achmada Fiqri A Rasyad on 04/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CourseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
